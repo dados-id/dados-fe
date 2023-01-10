d263a9b507509ea1b27d83d94a3a6f6bbe2429e4
marcellinuselbert <elbertmarcellinus@gmail.com>
2023-01-10 11:03:06 +0700

    Typography component done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        fontFamily: {
+            hauora: ["Hauora Sans", "sans-serif"],
+        },
+        screens: {
+            mobile: "360px",
+            tablet: "768px",
+            desktop: "994px",
+            wide: "1537px",
+        },
+        extend: {},
+    },
+    plugins: [],
 }
