ee79a8eed92b381da0a5b049041346d405b8a03d
= <stefan19052003@gmail.com>
2023-01-10 17:51:09 +0700

    edit nav and foot

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,3 +1,18 @@
 export const Footer = () => {
-  return <div>Nav</div>;
+  return (
+    <div className="w-full h-72 bg-blue-600 flex flex-col justify-center px-24">
+      <div className="bg-white h-1.5"></div>
+      <div className="flex flex-row justify-between items-end text-white">
+        {/* Left part */}
+        <div>
+          <h1>dados</h1>
+        </div>
+        {/* Right part */}
+        <div>
+
+        </div>
+      </div>
+
+    </div>
+  );
 };
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,3 +1,22 @@
+import Link from "next/link";
+
 export const Navbar = () => {
-  return <div>NHelo</div>
+  return(
+    <div className="h-16 w-full bg-slate-400 flex flex-row items-center justify-around">
+      {/* Logo */}
+      <div>
+        <div>dados</div>
+      </div>
+      {/* Nav */}
+      <nav className="flex flex-row gap-6">
+        <Link href="/howto">How to Rate</Link>
+        <Link href="/faq">FAQ</Link>
+        <Link href="/about">About Us</Link>
+      </nav>
+      {/* User */}
+      <div>
+
+      </div>
+    </div>
+  )
 }
