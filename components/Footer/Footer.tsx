57b9c1090dbfb50b2124210f2069c3c5228a9407
= <stefan19052003@gmail.com>
2023-01-15 21:43:37 +0700

    responsive navbar

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -2,9 +2,10 @@ import { Body, Header } from "@components"
 
 export const Footer = () => {
   return (
-    <div className="w-full h-72 bg-cobalt flex flex-col justify-center px-24">
-      <div className="bg-white h-1.5"></div>
-      <div className="flex flex-row justify-between items-end text-white mt-8">
+    <div className="w-full h-36 tablet:h-72 bg-cobalt flex flex-col justify-center px-8 tablet:px-24">
+      <div className="bg-white mobile:h-1 tablet:h-1.5"></div>
+      {/* Desktop and Tablet */}
+      <div className="hidden tablet:flex flex-row justify-between items-end text-white mt-8">
         {/* Left part */}
         <div className="w-1/2 ">
           <Header preset="h3">dados</Header>
@@ -15,7 +16,15 @@ export const Footer = () => {
           <Header preset="h5">© 2023 Dados Indonesia. All Rights Reserved</Header>
         </div>
       </div>
+      {/* Mobile */}
+      <div className="flex tablet:hidden flex-row justify-center items-center text-white mt-3">
+        {/* Left part */}
+        <div className="flex flex-col items-center ">
+          <Header preset="h1">dados</Header>
+          <Header preset="h6" className="text-center">© 2023 Dados Indonesia. All Rights Reserved</Header>
 
+        </div>
+      </div>
     </div>
   );
 };
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,35 +1,133 @@
 import Link from "next/link";
 import { Header, Body, Button } from "@components"
+import { useState } from "react";
 
 export const Navbar = () => {
-  return(
-    <div className="h-16 w-full bg-whipcream text-mariana flex flex-row items-center justify-around">
-      {/* Logo */}
-      <div>
-        <Header preset="h5" >dados</Header>
-      </div>
-      {/* Nav */}
-      <nav className="flex flex-row gap-8">
-        <Header preset="h5"><Link href="/howto">How to Rate</Link></Header>
-        <Header preset="h5"><Link href="/faq">FAQ</Link></Header>
-        <Header preset="h5"><Link href="/about">About Us</Link></Header>
-      </nav>
-      {/* User */}
-      <div>
-        <Button 
-          preset="primary" 
-          className="flex flex-row"
-          rightIcon={
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-            </svg>
+  const [menuOpen, setMenuOpen] = useState(false);
 
-        }
+  return (
+    <>
+      {/* Desktop */}
+      <div className="mobile:hidden tablet:hidden desktop:flex h-16 w-full bg-whipcream text-mariana flex-row items-center justify-between px-32">
+        {/* Logo */}
+        <div className="flex flex-row gap-12">
+          <Header preset="h5" >dados</Header>
+          {/* Nav */}
+          <nav className="flex flex-row gap-8">
+            <Header preset="h5"><Link href="/howto">How to Rate</Link></Header>
+            <Header preset="h5"><Link href="/faq">FAQ</Link></Header>
+            <Header preset="h5"><Link href="/about">About Us</Link></Header>
+          </nav>
+        </div>
+
+        {/* User */}
+        <div>
+          <Button
+            preset="primary"
+            className="flex flex-row"
+            rightIcon={
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+              </svg>
+
+            }
           >
             Login
 
-        </Button>
+          </Button>
+        </div>
       </div>
-    </div>
+      {/* Tablet */}
+      <div className="mobile:hidden tablet:flex desktop:hidden h-16 w-full bg-whipcream text-mariana flex-row items-center justify-around">
+        {/* Logo */}
+        <div>
+          <Header preset="h5" >dados</Header>
+        </div>
+        {/* Nav */}
+        <nav className="flex flex-row gap-8">
+          <Header preset="h5"><Link href="/howto">How to Rate</Link></Header>
+          <Header preset="h5"><Link href="/faq">FAQ</Link></Header>
+          <Header preset="h5"><Link href="/about">About Us</Link></Header>
+        </nav>
+        {/* User */}
+        <div>
+          <Button
+            preset="primary"
+            className="flex flex-row"
+            rightIcon={
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+              </svg>
+
+            }
+          >
+            Login
+
+          </Button>
+        </div>
+      </div>
+      {/* Mobile */}
+      <div className="mobile:flex tablet:hidden w-full text-mariana flex-col">
+        <div className="bg-whipcream h-16 w-full flex flex-row items-center justify-between px-8">
+          {/* Logo */}
+          <div>
+            <Header preset="h2" >dados</Header>
+          </div>
+          {/* User */}
+          <div>
+            <Button
+              preset="primary"
+              className="flex flex-row"
+              onClick={() => setMenuOpen(!menuOpen)}
+            >
+              { menuOpen ? 
+                (
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                </svg>
+                )
+                :
+                (
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+                )
+              }
+
+
+            </Button>
+
+          </div>
+        </div>
+        <div className={`${menuOpen ? 'h-0 opacity-0': 'opacity-100 py-4'}
+           transition-height ease-in-out duration-500 px-8 bg-whipcream`}
+          >
+          <nav className="flex flex-col gap-8">
+            <Header preset="h5"><Link href="/howto">How to Rate</Link></Header>
+            <Header preset="h5"><Link href="/faq">FAQ</Link></Header>
+            <Header preset="h5"><Link href="/about">About Us</Link></Header>
+          </nav>
+          {/* User */}
+          <div className="flex justify-end">
+            <Button
+              preset="primary"
+              padding=""
+              className="flex flex-row "
+              rightIcon={
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                </svg>
+
+              }
+            >
+              Login
+
+            </Button>
+          </div>
+        </div>
+      </div>
+
+
+    </>
   )
 }
