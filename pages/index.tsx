ce2dcd83661d9a37f1fb7a64d4c244e29010e4fb
marcellinuselbert <elbertmarcellinus@gmail.com>
2023-01-20 23:11:42 +0700

    Fix empty white space at the bottom of the page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,18 +4,16 @@ import LandingPatternRight from "@images/img_landing_pattern_right.svg"
 import BuildingImage from "@images/img_building.svg"
 export default function Home() {
     return (
-        <div className="wide:h-[70vh] desktop:h-screen">
-            <LandingPatternLeft className="absolute  desktop:w-full h-full -z-10" />
+        <div className="desktop:h-screen ">
+            <LandingPatternLeft className="absolute  desktop:w-full -z-10" />
             <LandingPatternRight className="absolute  -z-10 right-0" />
 
-            <div className="">
-                <div className="flex justify-center">
-                    <div className="h-[90px] w-[970px] bg-grey-600 mt-7"></div>
-                </div>
-                <div className="relative">
-                    <LandingText />
-                    <BuildingImage className="absolute desktop:right-20 desktop:-top-20 wide:right-60 wide:-top-28" />
-                </div>
+            <div className="flex justify-center">
+                <div className="h-[90px] w-[970px] bg-grey-600 mt-7"></div>
+            </div>
+            <div className="relative">
+                <LandingText />
+                <BuildingImage className="absolute desktop:right-20 desktop:-top-20 wide:right-60 wide:-top-28" />
             </div>
         </div>
     )
