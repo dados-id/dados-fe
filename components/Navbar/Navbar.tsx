d606551408658a84e263b7521ac3e9b5b10c8b79
= <stefan19052003@gmail.com>
2023-01-27 00:28:54 +0700

    add conditional and fix sizing

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link"
 import { Header, Body, Button } from "@components"
 import { useState } from "react"
+import LoginButton from "./LoginButton"
 
 export const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false)
@@ -8,12 +9,15 @@ export const Navbar = () => {
     return (
         <>
             {/* Desktop */}
-            <div className="mobile:hidden tablet:hidden desktop:flex h-16 w-full shadow-md bg-white text-mariana flex-row items-center justify-between px-32">
+            <div className="mobile:hidden tablet:flex h-16 w-full shadow-md bg-white text-mariana flex-row items-center justify-between px-32">
                 {/* Logo */}
-                <div className="flex flex-row gap-12">
-                    <Header preset="h5">dados</Header>
+                <div className="flex flex-row gap-10">
+                    <Link href="/">
+                        <img src="logo.svg"/>
+                    </Link>
+                    
                     {/* Nav */}
-                    <nav className="flex flex-row gap-8">
+                    <nav className="flex flex-row items-center justify-center gap-8">
                         <Header preset="h5">
                             <Link href="/howto">How to Rate</Link>
                         </Header>
@@ -28,81 +32,16 @@ export const Navbar = () => {
 
                 {/* User */}
                 <div>
-                    <Button
-                        preset="primary"
-                        className="flex flex-row"
-                        rightIcon={
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke-width="1.5"
-                                stroke="currentColor"
-                                className="w-6 h-6"
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    d="M8.25 4.5l7.5 7.5-7.5 7.5"
-                                />
-                            </svg>
-                        }
-                    >
-                        Login
-                    </Button>
-                </div>
-            </div>
-            {/* Tablet */}
-            <div className="mobile:hidden tablet:flex desktop:hidden h-16 w-full shadow-md bg-white text-mariana flex-row items-center justify-around">
-                {/* Logo */}
-                <div>
-                    <Header preset="h5">dados</Header>
-                </div>
-                {/* Nav */}
-                <nav className="flex flex-row gap-8">
-                    <Header preset="h5">
-                        <Link href="/howto">How to Rate</Link>
-                    </Header>
-                    <Header preset="h5">
-                        <Link href="/faq">FAQ</Link>
-                    </Header>
-                    <Header preset="h5">
-                        <Link href="/about">About Us</Link>
-                    </Header>
-                </nav>
-                {/* User */}
-                <div>
-                    <Button
-                        preset="primary"
-                        className="flex flex-row"
-                        rightIcon={
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke-width="1.5"
-                                stroke="currentColor"
-                                className="w-6 h-6"
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    d="M8.25 4.5l7.5 7.5-7.5 7.5"
-                                />
-                            </svg>
-                        }
-                    >
-                        Login
-                    </Button>
+                    <LoginButton user={{name: "ste"}}/>
                 </div>
             </div>
             {/* Mobile */}
             <div className="mobile:flex tablet:hidden w-full text-mariana flex-col shadow-md">
                 <div className="bg-white h-16 w-full flex flex-row items-center justify-between px-8">
                     {/* Logo */}
-                    <div>
-                        <Header preset="h2">dados</Header>
-                    </div>
+                    <Link href="/">
+                        <img src="logo.svg"/>
+                    </Link>
                     {/* User */}
                     <div>
                         <Button
@@ -145,9 +84,8 @@ export const Navbar = () => {
                     </div>
                 </div>
                 <div
-                    className={`${
-                        menuOpen ? "h-0 opacity-0" : "opacity-100 py-4"
-                    }
+                    className={`${menuOpen ? "h-0 opacity-0" : "opacity-100 py-4"
+                        }
            transition-height ease-in-out duration-500 px-8 bg-white`}
                 >
                     <nav className="flex flex-col gap-8">
@@ -163,29 +101,7 @@ export const Navbar = () => {
                     </nav>
                     {/* User */}
                     <div className="flex justify-end">
-                        <Button
-                            preset="primary"
-                            padding=""
-                            className="flex flex-row "
-                            rightIcon={
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke-width="1.5"
-                                    stroke="currentColor"
-                                    className="w-6 h-6"
-                                >
-                                    <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        d="M8.25 4.5l7.5 7.5-7.5 7.5"
-                                    />
-                                </svg>
-                            }
-                        >
-                            Login
-                        </Button>
+                        <LoginButton user={{name: "ste"}}/>
                     </div>
                 </div>
             </div>
