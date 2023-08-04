7b218c864463bd64dd933810563ac4f61fb49b2c
marcellinuselbert <elbertmarcellinus@gmail.com>
2023-08-04 19:10:01 +0700

    Disable hi button

--- components/Navbar/LoginButton.tsx
+++ components/Navbar/LoginButton.tsx
@@ -7,7 +7,7 @@ const LoginButton = ({ user }: any) => {
     const { googleSignIn } = useAuthContext()
     return (
         <>
-            {user.firstName ? (
+            {/* {user.firstName ? (
                 <Button
                     preset="secondary"
                     className="flex flex-row"
@@ -30,31 +30,31 @@ const LoginButton = ({ user }: any) => {
                 >
                     Hi, {user.firstName}
                 </Button>
-            ) : (
-                <Button
-                    preset="primary"
-                    className="flex flex-row"
-                    rightIcon={
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke-width="1.5"
-                            stroke="currentColor"
-                            className="w-6 h-6"
-                        >
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="M8.25 4.5l7.5 7.5-7.5 7.5"
-                            />
-                        </svg>
-                    }
-                    onClick={() => googleSignIn()}
-                >
-                    Login
-                </Button>
-            )}
+            ) : ( */}
+            <Button
+                preset="primary"
+                className="flex flex-row"
+                rightIcon={
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                    >
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M8.25 4.5l7.5 7.5-7.5 7.5"
+                        />
+                    </svg>
+                }
+                onClick={() => googleSignIn()}
+            >
+                Login
+            </Button>
+            {/* )} */}
         </>
     )
 }
